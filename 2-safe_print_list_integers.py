68002d1944625adbcfaf94397c1725d387077ef0
profytimmy <oyewaletimmy01@yahoo.com>
2022-06-20 10:37:21 +0000

    dd

--- 2-safe_print_list_integers.py
+++ 2-safe_print_list_integers.py
@@ -4,7 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     a = 0
     for i in range(0, x):
         try:
-            if my_list[i] != int():
-                print("{:d}".format(my_list[i]), end="")
-                a += 1
-        except
+            print("{:d}".format(my_list[i]), end="")
+            a += 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return (a)
