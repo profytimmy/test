bae750b403a92fe74434c6747eaefa84b162bc84
profytimmy <oyewaletimmy01@yahoo.com>
2022-06-20 10:29:46 +0000

    do

---
+++ 2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    for i in range(0, x):
+        try:
+            if my_list[i] != int():
+                print("{:d}".format(my_list[i]), end="")
+                a += 1
+        except
